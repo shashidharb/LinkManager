29fe97e96c5cb248c466dcf0895ae632d3b35f9d
Shashidhar B <shashidhar.b@gmail.com>
2026-01-08 12:07:45 +0530

    Remove Firebase config from firebase.js
    Removed Firebase configuration details from firebase.js.

--- firebase.js
+++ firebase.js
@@ -4,15 +4,7 @@ import { getAuth, getReactNativePersistence, initializeAuth } from 'firebase/aut
 import { getFirestore } from 'firebase/firestore';
 import { Platform } from 'react-native';
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyBiMfPs-wBF7gadZPNOWu5Bxq9Uxh7SyPU",
-  authDomain: "linkmanager-6b8fa.firebaseapp.com",
-  projectId: "linkmanager-6b8fa",
-  storageBucket: "linkmanager-6b8fa.appspot.com",
-  messagingSenderId: "1068294389128",
-  appId: "1:1068294389128:ios:65ec90a6017b9c59750e3d"
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
